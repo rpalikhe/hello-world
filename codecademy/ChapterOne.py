52db783770a6513acc2378eeaba13e0147c89bc8
reenu <reenu.palikhe@gmail.com>
2017-11-04 21:59:52 -0400

    simple python commands

---
+++ codecademy/ChapterOne.py
@@ -0,0 +1,17 @@
+parrot = ("Norwegian Blue")
+print(len(parrot))
+print(parrot.lower())
+print(parrot.upper())
+
+pi = 3.14
+print(str(pi))
+
+print("spam" + " and" + " eggs")
+
+print("the value of pi is " + str(pi))
+
+string1 = "hello"
+string2 = "world"
+
+print("lets say %s to %s" % (string1, string2))
+
